a54db53a705d5599f73cc196bfc407f241f71253
Sibashis216 <Boyd3256@gmail.com>
2022-04-12 18:25:15 +0530

    update

--- src/Task3/Blocks.js
+++ src/Task3/Blocks.js
@@ -1,10 +1,11 @@
 import React from 'react'
-
+// this is a github
 function Blocks(props) {
     return (
         <div>
             <h1 style={{fontSize:"30px"}}>{props.heading}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim reprehenderit fugit quaerat omnis veniam est corrupti laboriosam, repellat tempore neque! Est mollitia cupiditate tempore molestiae dolorem nihil placeat eveniet nesciunt.</p>
+
         </div>
     )
 }
